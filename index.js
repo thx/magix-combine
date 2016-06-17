593cd4ccb1001846af447001a60c7b12d6d8e467
xinglie.lkf <xinglie.lkf@taobao.com>
2016-06-18 00:19:47 +0800

    参考seajs require分析，更健壮的依赖分析

--- index.js
+++ index.js
@@ -677,9 +677,246 @@ Processor.add('tmpl', function() {
         process: processTmpl
     };
 });
+Processor.add('require:parser', function() {
+    /**
+     * util-deps.js - The parser for dependencies
+     * ref: tests/research/parse-dependencies/test.html
+     * ref: https://github.com/seajs/crequire
+     */
+
+    function parseDependencies(s) {
+        if (s.indexOf('require') == -1) {
+            return [];
+        }
+        var index = 0,
+            peek, length = s.length,
+            isReg = 1,
+            modName = 0,
+            res = [];
+        var parentheseState = 0,
+            parentheseStack = [];
+        var braceState, braceStack = [],
+            isReturn;
+        while (index < length) {
+            readch();
+            if (isBlank()) {
+                if (isReturn && (peek == '\n' || peek == '\r')) {
+                    braceState = 0;
+                    isReturn = 0;
+                }
+            } else if (isQuote()) {
+                dealQuote();
+                isReg = 1;
+                isReturn = 0;
+                braceState = 0;
+            } else if (peek == '/') {
+                readch();
+                if (peek == '/') {
+                    index = s.indexOf('\n', index);
+                    if (index == -1) {
+                        index = s.length;
+                    }
+                } else if (peek == '*') {
+                    var i = s.indexOf('\n', index);
+                    index = s.indexOf('*/', index);
+                    if (index == -1) {
+                        index = length;
+                    } else {
+                        index += 2;
+                    }
+                    if (isReturn && i != -1 && i < index) {
+                        braceState = 0;
+                        isReturn = 0;
+                    }
+                } else if (isReg) {
+                    dealReg();
+                    isReg = 0;
+                    isReturn = 0;
+                    braceState = 0;
+                } else {
+                    index--;
+                    isReg = 1;
+                    isReturn = 0;
+                    braceState = 1;
+                }
+            } else if (isWord()) {
+                dealWord();
+            } else if (isNumber()) {
+                dealNumber();
+                isReturn = 0;
+                braceState = 0;
+            } else if (peek == '(') {
+                parentheseStack.push(parentheseState);
+                isReg = 1;
+                isReturn = 0;
+                braceState = 1;
+            } else if (peek == ')') {
+                isReg = parentheseStack.pop();
+                isReturn = 0;
+                braceState = 0;
+            } else if (peek == '{') {
+                if (isReturn) {
+                    braceState = 1;
+                }
+                braceStack.push(braceState);
+                isReturn = 0;
+                isReg = 1;
+            } else if (peek == '}') {
+                braceState = braceStack.pop();
+                isReg = !braceState;
+                isReturn = 0;
+            } else {
+                var next = s.charAt(index);
+                if (peek == ';') {
+                    braceState = 0;
+                } else if (peek == '-' && next == '-' || peek == '+' && next == '+' || peek == '=' && next == '>') {
+                    braceState = 0;
+                    index++;
+                } else {
+                    braceState = 1;
+                }
+                isReg = peek != ']';
+                isReturn = 0;
+            }
+        }
+        return res;
+
+        function readch() {
+            peek = s.charAt(index++);
+        }
+
+        function isBlank() {
+            return /\s/.test(peek);
+        }
+
+        function isQuote() {
+            return peek == '"' || peek == "'";
+        }
+
+        function dealQuote() {
+            var start = index;
+            var c = peek;
+            var end = s.indexOf(c, start);
+            if (end == -1) {
+                index = length;
+            } else if (s.charAt(end - 1) != '\\') {
+                index = end + 1;
+            } else {
+                while (index < length) {
+                    readch();
+                    if (peek == '\\') {
+                        index++;
+                    } else if (peek == c) {
+                        break;
+                    }
+                }
+            }
+            if (modName) {
+                //maybe substring is faster  than slice .
+                //res.push(s.substring(start, index - 1));
+                res.push({
+                    name: s.substring(start, index - 1),
+                    start: start
+                });
+                modName = 0;
+            }
+        }
+
+        function dealReg() {
+            index--;
+            while (index < length) {
+                readch();
+                if (peek == '\\') {
+                    index++;
+                } else if (peek == '/') {
+                    break;
+                } else if (peek == '[') {
+                    while (index < length) {
+                        readch();
+                        if (peek == '\\') {
+                            index++;
+                        } else if (peek == ']') {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        function isWord() {
+            return /[a-z_$]/i.test(peek);
+        }
+
+        function dealWord() {
+            var s2 = s.slice(index - 1);
+            var r = /^[\w$]+/.exec(s2)[0];
+            parentheseState = {
+                'if': 1,
+                'for': 1,
+                'while': 1,
+                'with': 1
+            }[r];
+            isReg = {
+                'break': 1,
+                'case': 1,
+                'continue': 1,
+                'debugger': 1,
+                'delete': 1,
+                'do': 1,
+                'else': 1,
+                'false': 1,
+                'if': 1,
+                'in': 1,
+                'instanceof': 1,
+                'return': 1,
+                'typeof': 1,
+                'void': 1
+            }[r];
+            isReturn = r == 'return';
+            braceState = {
+                'instanceof': 1,
+                'delete': 1,
+                'void': 1,
+                'typeof': 1,
+                'return': 1
+            }.hasOwnProperty(r);
+            modName = /^require\s*(?:\/\*[\s\S]*?\*\/\s*)?\(\s*(['"]).+?\1\s*[),]/.test(s2);
+            if (modName) {
+                r = /^require\s*(?:\/\*[\s\S]*?\*\/\s*)?\(\s*['"]/.exec(s2)[0];
+                index += r.length - 2;
+            } else {
+                index += /^[\w$]+(?:\s*\.\s*[\w$]+)*/.exec(s2)[0].length - 1;
+            }
+        }
+
+        function isNumber() {
+            return /\d/.test(peek) || peek == '.' && /\d/.test(s.charAt(index));
+        }
+
+        function dealNumber() {
+            var s2 = s.slice(index - 1);
+            var r;
+            if (peek == '.') {
+                r = /^\.\d+(?:E[+-]?\d*)?\s*/i.exec(s2)[0];
+            } else if (/^0x[\da-f]*/i.test(s2)) {
+                r = /^0x[\da-f]*\s*/i.exec(s2)[0];
+            } else {
+                r = /^\d+\.?\d*(?:E[+-]?\d*)?\s*/i.exec(s2)[0];
+            }
+            index += r.length - 1;
+            isReg = 0;
+        }
+    }
+
+    return {
+        process: parseDependencies
+    };
+});
 Processor.add('require', function() {
-    var depsReg = /(?:var\s+([^=]+)=\s*)?(\.)?\brequire\s*\(([^\(\)]+)\);?/g;
+    var depsReg = /(?:var\s+([^=]+)=\s*)?\brequire\s*\(([^\(\)]+)\);?/g;
     var exportsReg = /module\.exports\s*=\s*/;
+    var anchor = '\u0011';
+    var anchorReg = /(['"])\u0011([^'"]+)\1/;
     return {
         process: function(e) {
             var deps = [];
@@ -691,8 +928,15 @@ Processor.add('require', function() {
                 e.content = e.content.replace(exportsReg, 'return ');
                 hasExports = true;
             }
-            e.content = e.content.replace(depsReg, function(match, key, dot, str) {
-                if (dot) return match; //以.开始的require
+            var depsInfo = Processor.run('require:parser', 'process', [e.content]);
+            for (var i = 0, start; i < depsInfo.length; i++) {
+                start = depsInfo[i].start + i;
+                e.content = e.content.substring(0, start) + anchor + e.content.substring(start);
+            }
+            e.content = e.content.replace(depsReg, function(match, key, str) {
+                var info = str.match(anchorReg);
+                if (!info) return match;
+                str = info[1] + info[2] + info[1];
                 str = resolveAtPath(str, moduleId);
                 if (key) {
                     vars.push(key);
@@ -700,7 +944,7 @@ Processor.add('require', function() {
                 } else {
                     noKeyDeps.push(str);
                 }
-                return configs.removeRequire ? '' : match;
+                return configs.removeRequire ? '' : match.replace(anchor, '');
             });
             deps = deps.concat(noKeyDeps);
             e.moduleId = moduleId;
@@ -713,31 +957,31 @@ Processor.add('require', function() {
     };
 });
 
-Processor.add('comment', function() {
-    var anchor = '~\u0011';
-    var comment = /("([^\\"]*(\\.)?)*")|('([^\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n))|(\/\*(\n|.)*?\*\/)/g;
-    var lineCommentStart = /^\/{2,}/;
-    var mlCommentStart = /^\/\*/;
-    var key = /\~\u0011\d+/g;
-    return {
-        remove: function(content, store) {
-            var idx = 0;
-            return content.replace(comment, function(m) {
-                if (lineCommentStart.test(m) || mlCommentStart.test(m)) {
-                    var key = anchor + idx++;
-                    store[key] = m;
-                    return key;
-                }
-                return m;
-            });
-        },
-        restore: function(content, store) {
-            return content.replace(key, function(m) {
-                return store[m];
-            });
-        }
-    };
-});
+// Processor.add('comment', function() {
+//     var anchor = '~\u0011';
+//     var comment = /("(?:[^\\"]*(?:\\.)?)*")|('(?:[^\\']*(?:\\.)?)*')|(\/{2,}.*?[\r\n])|(\/\*(?:\n|\r|.)*?\*\/)/g;
+//     var lineCommentStart = /^\/{2,}/;
+//     var mlCommentStart = /^\/\*/;
+//     var key = /\~\u0011\d+/g;
+//     return {
+//         remove: function(content, store) {
+//             var idx = 0;
+//             return content.replace(comment, function(m) {
+//                 if (lineCommentStart.test(m) || mlCommentStart.test(m)) {
+//                     var key = anchor + idx++;
+//                     store[key] = m;
+//                     return key;
+//                 }
+//                 return m;
+//             });
+//         },
+//         restore: function(content, store) {
+//             return content.replace(key, function(m) {
+//                 return store[m];
+//             });
+//         }
+//     };
+// });
 Processor.add('file', function() {
     var moduleIdReg = /(['"])(@moduleId)\1/g;
     var extnames = {
@@ -756,8 +1000,8 @@ Processor.add('file', function() {
         }
         if (jsReg.test(from)) {
             copyFile(from, to, function(content) {
-                var store = {};
-                content = Processor.run('comment', 'remove', [content, store]);
+                //var store = {};
+                //content = Processor.run('comment', 'remove', [content, store]);
                 return new Promise(function(resolve) {
                     Processor.run('require', 'process', [{
                         from: from,
@@ -768,7 +1012,7 @@ Processor.add('file', function() {
                     }).then(function(e) {
                         return Processor.run('tmpl', 'process', [e]);
                     }).then(function(e) {
-                        e.content = Processor.run('comment', 'restore', [e.content, store]);
+                        //e.content = Processor.run('comment', 'restore', [e.content, store]);
                         e.content = e.content.replace(moduleIdReg, '$1' + e.moduleId + '$1');
                         e.content = resolveAtPath(e.content, e.moduleId);
                         var tmpl = configs.generateJSFile(e);
